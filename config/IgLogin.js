2e02eaf99ba917acb859ab609c2c581c88fb0bfe
yudax42 <yudax42@gmail.com>
2021-08-10 07:57:13 +0100

    hide prelogin flow

--- config/IgLogin.js
+++ config/IgLogin.js
@@ -6,7 +6,7 @@ let _ig;
 const login = async () => {
   try {
     ig.state.generateDevice(process.env.IG_USERNAME);
-    await ig.simulate.preLoginFlow();
+    // await ig.simulate.preLoginFlow();
     await ig.account.login(process.env.IG_USERNAME, process.env.IG_PASSWORD);
     _ig = ig;
   } catch (err) {
